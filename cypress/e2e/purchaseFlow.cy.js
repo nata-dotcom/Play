0f253889e8fafc223b6c49be9e12ee4a1f4f6be5
nata <nat.mishchuk@gmail.com>
2023-11-07 16:45:05 +0100

    refactored and finished flows

--- cypress/e2e/purchaseFlow.cy.js
+++ cypress/e2e/purchaseFlow.cy.js
@@ -1,8 +1,10 @@
 
-describe('open homepage, PLP, search for item', () => {
+describe('open homepage, PDP, add to cart and go to checkout', () => {
   beforeEach(() => {
     cy.visit('/')
+    cy.intercept('POST','**/checkout/cart/add/**').as('toCart')
     cy.fixture('pdp.json').as('elementsPdp')
+    cy.fixture('cart.json').as('elementsCart')
     cy.fixture('homepage.json').as('elements')
     cy.get('@elements').then((element)=>{
       cy.clickOnButton(element.cookieBlock,element.cookieAccept)
@@ -17,10 +19,14 @@ describe('open homepage, PLP, search for item', () => {
     cy.get(element.relatedProducts).should('be.visible')
     cy.get(element.addToCart).should('be.enabled').click()
    })   
+   cy.wait('@toCart').its('response.statusCode').should('eq', 200)
    cy.visit('/checkout/cart')
-   cy.get('div.cart-empty').should('not.exist')
-   cy.get('.cart.item').should('be.visible')
-   cy.get('ul.checkout.methods.items.checkout-methods-items>li>button').should('be.enabled').click()
+   cy.get('@elementsCart').then((element)=>{
+    cy.get(element.emptyCart).should('not.exist')
+    cy.get(element.cartItem).should('be.visible')
+    cy.get(element.toCheckoutButton).should('be.enabled').click()
+   })
+   
    cy.visit('/checkout/klarna')
    cy.url().then((url =>{
     expect(url).to.contain('checkout/klarna')
